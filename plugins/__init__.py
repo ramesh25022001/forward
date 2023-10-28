208460760ab08b71f1fff51389c0db48bef2bb7b
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2023-10-28 19:32:24 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,7 +8,7 @@ import sys
 
 
 @bot.on_message(filters.me & filters.command("start", prefixes=prefixes))
-async def Start_msg(bot: Client , m: Message):
+async def Start_msg(bot: bot , m: Message):
     await m.reply_text(
     caption = f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n" +
     f"\nI am Auto Forwarder bot." +
