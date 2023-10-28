799a71f5f311d06c35a326dc739de1a93af69231
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2023-10-28 19:23:40 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message
-from main import LOGGER, prefixes, AUTH_USERS
+from main import LOGGER, prefixes, AUTH_USERS, bot
 from config import Config
 import time
 import os
